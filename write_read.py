3c43cb5d3a77c4303300fac5bb21e2cd229711cd
vlad-kraf <vladkravchenkomail@gmail.com>
2019-04-04 22:27:22 +0300

    write_read_v1

--- write_read.py
+++ write_read.py
@@ -1,3 +1,23 @@
+class BookIOErrors(Exception):
+    pass
+
+
+class PageNotFoundError(BookIOErrors):
+    pass
+
+
+class TooLongTextError(BookIOErrors):
+    pass
+
+
+class PermissionDeniedError(BookIOErrors):
+    pass
+
+
+class NotExistingExtensionError(BookIOErrors):
+    pass
+
+
 class Book:
     def __init__(self, title, content=None):
         self.title = title
@@ -14,11 +34,10 @@ class Book:
 class Novel(Book):
     """класс описывающий книгу и методы работы с ней"""
 
-    def __init__(self, author, year, title, content):
+    def __init__(self, author, year, title, content=None):
         """конструктор"""
         bookmark = {}
-        self.title = title
-        self.content = content
+        super().__init__(title, content)
         self.size = len(self.content)
         self.author = author
         self.year = year
@@ -26,18 +45,23 @@ class Novel(Book):
 
     def read(self, page):
         """возвращает страницу"""
+        return self.content[page]
 
     def set_bookmark(self, person, page):
         """устанавливает закладку в книгу book"""
+        self.bookmark[person] = page
 
     def get_bookmark(self, person):
         """получает номер страницы установленной закладки в книге book"""
+        return self.bookmark[person]
 
     def del_bookmark(self, person):
         """удаляет закладку читателя person, если она установлена"""
+        del self.bookmark[person]
 
     def write(self, page, text):
         """делает запись текста text на страницу page """
+        self.content[page] = text
 
 
 class Notebook(Book):
@@ -45,16 +69,18 @@ class Notebook(Book):
 
     def __init__(self, title, size, max_sign, content):
         """конструктор"""
-        self.title = title
+        super().__init__(title, content)
         self.size = len(self.content)
         self.max_sign = max_sign
-        self.content = content
+
 
     def read(self, page):
         """возвращает страницу с номером page"""
+        return self.content[page]
 
     def write(self, page, text):
         """делает запись текста text на страницу с номером page """
+        self.content[page] = text
 
 
 class Person:
@@ -66,15 +92,20 @@ class Person:
 
     def read(self, book, page):
         """читаем страницу с номером page в книге book"""
+        return book.read(page)
 
     def write(self, book, page, text):
         """пишем на страницу с номером page в книге book"""
+        book.write(page, text)
 
     def set_bookmark(self, book, page):
         """устанавливаем закладку в книгу book на страницу с номером page"""
+        book.set_bookmark(self.name, page)
 
     def get_bookmark(self, book):
         """получаем номер страницы установленной закладки в книге book"""
+        return book.get_bookmark(self.name)
 
     def del_bookmark(self, book):
         """удаляет закладку из книги book"""
+        book.del_bookmark(self.name)
