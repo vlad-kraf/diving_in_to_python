6673ba7cc23a3b1db8b53c66b974d2c8486f70d0
vlad-kraf <vladkravchenkomail@gmail.com>
2019-04-21 22:35:32 +0300

    file_interface_v1

---
+++ file_interface.py
@@ -0,0 +1,34 @@
+import tempfile
+import os
+
+class File:
+    def __init__(self, path, content = None):
+        self.path = path
+        self.content = content
+
+    def write(self, content):
+        self.content = content
+        with open(self.path, 'w') as fp:
+            fp.write(self.content)
+
+    def read(self):
+        with open(self.path, 'r') as fp:
+            output = fp.read()
+        print(output)
+
+    def __add__(self, second):
+        """В этом случае создается новый файл и файловый объект, в котором содержимое
+        второго файла добавляется к содержимому первого файла. Новый файл должен
+        создаваться в директории, полученной с помощью tempfile.gettempdir.
+        Для получения нового пути можно использовать os.path.join."""
+
+        new_path = tempfile.gettempdir() + self.path + second.path
+
+        with open(new_path, 'w') as fp:
+            fp.write(self.content + second.content)
+
+
+    def __str__(self):
+        """с помощью функции print должен печататься его полный путь, переданный при
+        инициализации."""
+        return self.path
--- test2.py
+++ test2.py
@@ -1,3 +1,8 @@
-print("test_merge")
-print("test_merge")
-print("test_merge")
+import tempfile
+import os
+path = 'zzzzz'
+
+
+new_path = os.path.join(tempfile.gettempdir(), path)
+
+print(new_path)
