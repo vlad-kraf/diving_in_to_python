8993f5e758a1297a089febda132fb4405ee6b3ec
vlad-kraf <vladkravchenkomail@gmail.com>
2019-04-30 01:54:40 +0300

    work_on_bugs_3_V1

--- work_on_bugs_2.py
+++ work_on_bugs_2.py
@@ -1,6 +1,5 @@
 import calendar
 
-
 class TooLongTextError(Exception):
     pass
 
@@ -16,7 +15,7 @@ class Page:
         self._text = '' if text is None else text
         self.max_sign = max_sign
 
-    # напишите вашу реализацию методов здесь
+    # напишите вашу реализацию методов здесь   
     def __iadd__(self, other):
 
         if type(self) not in (Page, str) or type(other) not in (Page, str):
@@ -25,13 +24,14 @@ class Page:
         if len(self) + len(other) > self.max_sign:
             raise TooLongTextError
 
+        
         return self + other
 
     def __add__(self, other):
-
+        
         if len(self) + len(other) > self.max_sign:
             raise TooLongTextError
-
+        
         self._text = self._text + other
 
         return self
@@ -54,18 +54,18 @@ class Page:
         if len(self._text) >= len(other):
             return True
         return False
-
+    
     def __eq__(self, other):
         if len(self) == len(other):
             return True
         return False
-
+    
     def __str__(self):
         return self._text
 
     def __repr__(self):
         return self._text
-
+    
     def __len__(self):
         return len(self._text)
 
@@ -79,14 +79,15 @@ class Book(object):
 
     def __len__(self):
         return len(self._content)
-
+    
     def __getitem__(self, key):
         if key < 1 or key > len(self._content):
             raise PageNotFoundError
-        return self._content[key - 1]
+        return self._content[key-1]
 
     def __setitem__(self, key, value):
-        self._content[key - 1] = Page(str(value))
+        self._content[key-1] = Page(str(value))
+
 
     def __gt__(self, other):
         if len(self._content) > len(other._content):
@@ -102,12 +103,12 @@ class Book(object):
         if len(self._content) >= len(other._content):
             return True
         return False
-
+    
     def __eq__(self, other):
 
         if type(self) != Book or type(other) != Book:
             raise TypeError
-
+        
         if len(self._content) == len(other._content):
             return True
         return False
@@ -115,7 +116,6 @@ class Book(object):
 
 class CalendarBookmark:
     """класс дескриптор - закладка для ежедневника"""
-
     def __init__(self, value=0):
         self.value = int(value)
 
@@ -125,22 +125,87 @@ class CalendarBookmark:
     def __set__(self, instance, value):
         self.value = value
 
+    
+
+
+
+# часть 3 --------------------------------------------------------------------------------------------------------------
+
+class Person:
+    """класс описывающий человека"""
+
+    def __init__(self, name):
+        self.name = name
+
+    def set_bookmark(self, *args, **kwargs):
+        pass
+
+    def get_bookmark(self, *args, **kwargs):
+        pass
+
+    def del_bookmark(self, *args, **kwargs):
+        pass
+
+
+class Reader:
+    def read(self, book, num_page):
+        pass
+
+
+class Writer:
+    def write(self, book, num_page, text):
+        pass
+
+
+class AdvancedPerson(Person, Reader, Writer):
+    """класс человека умеющего читать, писать, пользоваться закладками"""
+
+    def set_bookmark(self, book, num_page):
+        pass
+
+    def get_bookmark(self, book):
+        pass
+
+    def del_bookmark(self, book, person):
+        pass
+
+    def search(book, page):
+        pass
+
+    def read(self, book, page):
+        pass
+
+    def write(self, book, page, text):
+        pass
+
+
+class PageTableContents(Page):
+
+    def __init__(self, text=None, max_sign=2000):
+        pass
+
+    def search(self, chapter):
+        pass
+
 
 class CalendarBook(Book):
     """класс книги - ежедневник с закладкой"""
 
-    bookmark = CalendarBookmark()  # присваиваем дескриптор переменной с закаладками
+    bookmark = CalendarBookmark() # присваиваем дескриптор переменной с закаладками
 
     def __init__(self, title, content=None):
         super(CalendarBook, self).__init__(title, content)
+        table_of_contents = 'TABLE OF CONTENT\n'
 
-        # генерируем страницы жедневника на заданный год.
+        #генерируем страницы жедневника на заданный год.
         for month in range(1, 13):
-            # Добавляем календарь на первую страницу для каждого месяца в ежедневнике.
-            self._content.append(Page(str(calendar.TextCalendar(calendar.MONDAY).formatmonth(int(self.title), month))))
+            #Добавляем календарь на первую страницу для каждого месяца в ежедневнике.
+            self._content.append(Page(str(calendar.TextCalendar(calendar.SUNDAY).formatmonth(int(self.title), month))))
+            table_of_contents += calendar.month_name[month] + ':' + str(len(self._content)) + '\n'
             for date in calendar.Calendar().itermonthdates(int(self.title), month):
-                # Проверяем соответствует ли номер месяца месяцу для которого нужно создать
-                # страницы по дням в ежедневнике
+                #Проверяем соответствует ли номер месяца месяцу для которого нужно создать страницы
+                # по дням в ежедневнике
                 if date.month == month:
-                    # создаем страницы дней по датам в ежедневнике
+                    #создаем страницы дней по датам в ежедневнике
                     self._content.append(Page(str(date)))
+        self._content.append(table_of_contents)
